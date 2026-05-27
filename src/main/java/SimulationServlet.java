c7755adb7e42deea00bf874ea0c19da6f3921ae9
Cauã Lima <cauai8099@gmail.com>
2026-05-26 22:28:15 -0300

    Adicionado o calculo da parcela

--- src/main/java/SimulationServlet.java
+++ src/main/java/SimulationServlet.java
@@ -19,6 +19,44 @@ public class SimulationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         // Formula = valorParcela = valorFinanciado x taxaJuros(1+taxaJuros)^quantParcelas / (1+taxaJuros)^quantParcelas - 1
+        double valorParcela = 0;
+
+        String veiculo = req.getParameter("valorVeiculo");
+        double valorVeiculo  = Double.parseDouble(veiculo);
+
+        String entrada = req.getParameter("valorEntrada");
+        double valorEntrada  = Double.parseDouble(entrada);
+
+        String taxa = req.getParameter("taxaJuros");
+        double taxaJuros = Double.parseDouble(taxa);
+
+        String prazo = req.getParameter("prazoFinanc");
+        double prazoFinanc = Double.parseDouble(prazo);
+
+        double valorFinanc = valorVeiculo - valorEntrada;
+        taxaJuros = taxaJuros / 100;
+
+        valorParcela = valorFinanc * (taxaJuros * Math.pow((1+taxaJuros), prazoFinanc)) / (Math.pow((1+taxaJuros), prazoFinanc) - 1);
+
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        resp.getWriter().println("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Resultado</title>
+        </head>
+        <body>
+
+            <h1>Resultado da Simulação</h1>
+
+            <h2>Valor da parcela: R$: """ + valorParcela + """
+            </h2>
+
+        </body>
+        </html>
+        """);
 
     }
 }
--- src/main/webapp/simulation.jsp
+++ src/main/webapp/simulation.jsp
@@ -73,16 +73,16 @@
     <h1>Simulação</h1>
 
     <label>Valor do veículo</label>
-    <input type="number" min="0" placeholder="R$ 0,00">
+    <input type="number" min="0" placeholder="R$ 0,00" name="valorVeiculo">
 
     <label>Valor da entrada</label>
-    <input type="number" min="0" placeholder="R$ 0,00">
+    <input type="number" min="0" placeholder="R$ 0,00" name="valorEntrada">
 
     <label>Taxa de juros (%)</label>
-    <input type="number" min="0" step="0.01" placeholder="0.00">
+    <input type="number" min="0" step="0.01" placeholder="0.00" name="taxaJuros">
 
     <label>Prazo do financiamento (meses)</label>
-    <input type="number" min="0" placeholder="Ex: 48">
+    <input type="number" min="0" placeholder="Ex: 48" name="prazoFinanc">
 
     <input type="submit" min="0" value="Simular">
 
